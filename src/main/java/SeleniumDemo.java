1e0ff46bb91df99787b976bc32f346d0e6c41edf
Be-Asia McKerracher <amerigirl29@gmail.com>
2025-04-26 09:28:09 -0700

    updated drivers for selenium

--- src/main/java/SeleniumDemo.java
+++ src/main/java/SeleniumDemo.java
@@ -1,7 +1,21 @@
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 public class SeleniumDemo {
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:/path/to/chromedriver.exe");
 
+        String chromeTitle = chromeTest("https://www.google.com/");
+        System.out.println(chromeTitle);
 
+    }
+
+    private static String chromeTest(String url){
+        WebDriver driver = new ChromeDriver();
+        driver.get(url);
+        String title = driver.getTitle();
+        driver.quit();
+        return title;
     }
 
 }
